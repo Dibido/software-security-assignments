def0a7de6b949535e18d31f8fcf55f31ee0b2630
Bram <brampellen@gmail.com>
2022-10-27 13:02:07 +0200

    Wrapper requires argument for file type, because file names generated by AFL don´t specify the file type

--- fuzzing/libgd-wrapper/main.c
+++ fuzzing/libgd-wrapper/main.c
@@ -1,49 +1,70 @@
 #include "libgd/src/gd.h"
 #include <string.h>
 
-#define NAME_BUF_SIZE 250
+#define TYPE_BUF_SIZE 5
+#define NAME_BUF_SIZE 500
 
 
 int main(int argc, char **argv) {
-    if (argc < 2) {
-        printf("Please specify an image to parse\n");
+    if (argc < 3) {
+        printf("Please specify a file type and an image to parse.\n");
         return 0;
     }
 
+    char fileType[TYPE_BUF_SIZE];
+    fileType[TYPE_BUF_SIZE-1] = '\0';
+    strncpy(fileType, argv[1], TYPE_BUF_SIZE-1);
+    printf("Specified file type: %s\n", fileType);
+
     char fileName[NAME_BUF_SIZE];
     fileName[NAME_BUF_SIZE-1] = '\0';
-    strncpy(fileName, argv[1], NAME_BUF_SIZE-1);
-    printf("File name: %s\n", fileName);
+    strncpy(fileName, argv[2], NAME_BUF_SIZE-1);
+    printf("Specified file name: %s\n", fileName);
 
     FILE *file = fopen(fileName, "rb");
-	if (file == NULL) {
+    if (file == NULL) {
         perror("Error opening file");
         return 1;
     }
 
-    char tmpFileName[NAME_BUF_SIZE];
-    strncpy(tmpFileName, fileName, NAME_BUF_SIZE-1);
-    char* base = strtok(tmpFileName, ".");
-    char* ext = strtok(NULL, " ");
-    printf("Extension: %s\n", ext);
-
-	gdImagePtr imgPtr;
-    if (strcmp(ext, "png") == 0) {
+    gdImagePtr imgPtr;
+    if (strcmp(fileType, "png") == 0) {
+        printf("Parsing file as png.\n");
         imgPtr = gdImageCreateFromPng(file);
     }
-    else if (strcmp(ext, "jpeg") == 0) {
+    else if (strcmp(fileType, "jpeg") == 0) {
+        printf("Parsing file as jpeg.\n");
         imgPtr = gdImageCreateFromJpeg(file);
     }
-    else if (strcmp(ext, "gif") == 0) {
+    else if (strcmp(fileType, "gif") == 0) {
+        printf("Parsing file as gif.\n");
         imgPtr = gdImageCreateFromGif(file);
-	}
-    else if (strcmp(ext, "tiff") == 0) {
+    }
+    else if (strcmp(fileType, "tiff") == 0) {
+        printf("Parsing file as tiff.\n");
         imgPtr = gdImageCreateFromTiff(file);
     }
+    else if (strcmp(fileType, "bmp") == 0) {
+        printf("Parsing file as bmp.\n");
+        imgPtr = gdImageCreateFromBmp(file);
+    }
+    else if (strcmp(fileType, "tga") == 0) {
+        printf("Parsing file as tga.\n");
+        imgPtr = gdImageCreateFromTga(file);
+    }
+    else if (strcmp(fileType, "wbmp") == 0) {
+        printf("Parsing file as wbmp.\n");
+        imgPtr = gdImageCreateFromWBMP(file);
+    }
     else {
-        printf("File extension %s is not supported\n", ext);
+        printf("File type %s is not supported.\n", fileType);
     }
     fclose(file);
+
+    // It seems that AFL never reports new finds if we don´t dereference imgPtr.
+    if (imgPtr == NULL) {
+        printf("Failed to parse image.\n");
+    }
 
     return 0;
 }
