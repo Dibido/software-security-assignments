4147231457602dab33434139ada0dac8b136be7e
Bram <brampellen@gmail.com>
2022-11-29 14:40:29 +0100

    Wrapper no longer tries to apply transformations when libgd failed to parse the image

--- fuzzing/libgd-wrapper/main.c
+++ fuzzing/libgd-wrapper/main.c
@@ -116,85 +116,86 @@ int main(int argc, char **argv) {
     // It seems that AFL never reports new finds if we don´t dereference imgPtr.
     if (imgPtr == NULL) {
         printf("Failed to parse image.\n");
-    }
-
-    // Apply transformations
-    if (transformationsAmount > 0) {
-        printf("Applying transformations:\n");
-        for (int i = 0; i < transformationsAmount; ++i) {
-            if (strcmp(transformations[i], "fliph") == 0) {
-                gdImageFlipHorizontal(imgPtr);
-                printf("  Flipped image horizontally.\n");
-            } else if (strcmp(transformations[i], "flipv") == 0) {
-                gdImageFlipVertical(imgPtr);
-                printf("  Flipped image vertically.\n");
-            } else if (strcmp(transformations[i], "flipb") == 0) {
-                gdImageFlipBoth(imgPtr);
-                printf("  Flipped image both horizontally and vertically.\n");
-            } else if (strcmp(transformations[i], "gblur") == 0) {
-                if (gdImageGaussianBlur(imgPtr) == GD_TRUE) {
-                    printf("  Applied Gaussian blur to image.\n");
-                } else {
-                    printf("  Failed to apply Gaussian blur to image.\n");
-                }
-            } else if (strcmp(transformations[i], "gray") == 0) {
-                if (gdImageGrayScale(imgPtr) == GD_TRUE) {
-                    printf("  Transformed image to grayscale.\n");
-                } else {
-                    printf("  Failed to transform image to grayscale.\n");
-                }
-            } else if (strcmp(transformations[i], "negate") == 0) {
-                if (gdImageNegate(imgPtr) == GD_TRUE) {
-                    printf("  Negated image.\n");
-                } else {
-                    printf("  Failed to negate image.\n");
-                }
-            } /*else if (strncmp("crop", transformations[i], 4) == 0) {
-                char args[4][TRANSFORMATION_ARG_VAL_BUF_SIZE];
-                if (separateTransformArgs(4,transformations[i], args) == 1) {
-                    gdRect rect;
-                    rect.x = atoi(args[0]);
-                    rect.y = atoi(args[1]);
-                    rect.width = atoi(args[2]);
-                    rect.height = atoi(args[3]);
-                    gdImagePtr croppedImgPtr = gdImageCrop(imgPtr, &rect);
-                    if (croppedImgPtr != NULL) {
-                        imgPtr = croppedImgPtr;
-                        printf("  Cropped a rectangle with a width of %d and a height of %d pixels from x-coordinate %d and y-coordinate %d of the image.\n",
-                                rect.width, rect.height, rect.x, rect.y);
+        return 0;
+    } else {
+        // Apply transformations
+        if (transformationsAmount > 0) {
+            printf("Applying transformations:\n");
+            for (int i = 0; i < transformationsAmount; ++i) {
+                if (strcmp(transformations[i], "fliph") == 0) {
+                    gdImageFlipHorizontal(imgPtr);
+                    printf("  Flipped image horizontally.\n");
+                } else if (strcmp(transformations[i], "flipv") == 0) {
+                    gdImageFlipVertical(imgPtr);
+                    printf("  Flipped image vertically.\n");
+                } else if (strcmp(transformations[i], "flipb") == 0) {
+                    gdImageFlipBoth(imgPtr);
+                    printf("  Flipped image both horizontally and vertically.\n");
+                } else if (strcmp(transformations[i], "gblur") == 0) {
+                    if (gdImageGaussianBlur(imgPtr) == GD_TRUE) {
+                        printf("  Applied Gaussian blur to image.\n");
                     } else {
-                        printf("  Failed to crop a rectangle with a width of %d and a height of %d pixels from x-coordinate %d and y-coordinate %d of the image.\n",
-                                rect.width, rect.height, rect.x, rect.y);
+                        printf("  Failed to apply Gaussian blur to image.\n");
                     }
-                }
-            }*/ else if (strncmp("scale", transformations[i], 5) == 0) {
-                char args[2][TRANSFORMATION_ARG_VAL_BUF_SIZE];
-                if (separateTransformArgs(2,transformations[i], args) == 1) {
-                    unsigned int width = (unsigned int)atoi(args[0]);
-                    unsigned int height = (unsigned int)atoi(args[1]);
-                    gdImagePtr scaledImgPtr = gdImageScale(imgPtr, width, height);
-                    if (scaledImgPtr != NULL) {
-                        imgPtr = scaledImgPtr;
-                        printf("  Scaled the image to have a width of %d and a height of %d pixels.\n", width, height);
+                } else if (strcmp(transformations[i], "gray") == 0) {
+                    if (gdImageGrayScale(imgPtr) == GD_TRUE) {
+                        printf("  Transformed image to grayscale.\n");
                     } else {
-                        printf("  Failed to scale the image to have a width of %d and a height of %d pixels.\n", width, height);
+                        printf("  Failed to transform image to grayscale.\n");
                     }
-                }
-            } else if (strncmp("rotate", transformations[i], 6) == 0) {
-                char args[2][TRANSFORMATION_ARG_VAL_BUF_SIZE];
-                if (separateTransformArgs(2,transformations[i], args) == 1) {
-                    float angle = atof(args[0]);
-                    int bgColor = atoi(args[1]);
-                    gdImagePtr rotatedImagePtr = gdImageRotateInterpolated(imgPtr, angle, bgColor);
-                    if (rotatedImagePtr != NULL) {
-                        imgPtr = rotatedImagePtr;
-                        printf("  Rotated the image with an angle of %f degrees and background color %d.\n", angle, bgColor);
+                } else if (strcmp(transformations[i], "negate") == 0) {
+                    if (gdImageNegate(imgPtr) == GD_TRUE) {
+                        printf("  Negated image.\n");
                     } else {
-                        printf("  Failed to rotate the image with an angle of %f degrees and background color %d.\n", angle, bgColor);
+                        printf("  Failed to negate image.\n");
                     }
+                } else if (strncmp("crop", transformations[i], 4) == 0) {
+                    char args[4][TRANSFORMATION_ARG_VAL_BUF_SIZE];
+                    if (separateTransformArgs(4,transformations[i], args) == 1) {
+                        gdRect rect;
+                        rect.x = atoi(args[0]);
+                        rect.y = atoi(args[1]);
+                        rect.width = atoi(args[2]);
+                        rect.height = atoi(args[3]);
+                        gdImagePtr croppedImgPtr = gdImageCrop(imgPtr, &rect);
+                        if (croppedImgPtr != NULL) {
+                            imgPtr = croppedImgPtr;
+                            printf("  Cropped a rectangle with a width of %d and a height of %d pixels from x-coordinate %d and y-coordinate %d of the image.\n",
+                                    rect.width, rect.height, rect.x, rect.y);
+                        } else {
+                            printf("  Failed to crop a rectangle with a width of %d and a height of %d pixels from x-coordinate %d and y-coordinate %d of the image.\n",
+                                    rect.width, rect.height, rect.x, rect.y);
+                        }
+                    }
+                } else if (strncmp("scale", transformations[i], 5) == 0) {
+                    char args[2][TRANSFORMATION_ARG_VAL_BUF_SIZE];
+                    if (separateTransformArgs(2,transformations[i], args) == 1) {
+                        unsigned int width = (unsigned int)atoi(args[0]);
+                        unsigned int height = (unsigned int)atoi(args[1]);
+                        gdImagePtr scaledImgPtr = gdImageScale(imgPtr, width, height);
+                        if (scaledImgPtr != NULL) {
+                            imgPtr = scaledImgPtr;
+                            printf("  Scaled the image to have a width of %d and a height of %d pixels.\n", width, height);
+                        } else {
+                            printf("  Failed to scale the image to have a width of %d and a height of %d pixels.\n", width, height);
+                        }
+                    }
+                } else if (strncmp("rotate", transformations[i], 6) == 0) {
+                    char args[2][TRANSFORMATION_ARG_VAL_BUF_SIZE];
+                    if (separateTransformArgs(2,transformations[i], args) == 1) {
+                        float angle = atof(args[0]);
+                        int bgColor = atoi(args[1]);
+                        gdImagePtr rotatedImagePtr = gdImageRotateInterpolated(imgPtr, angle, bgColor);
+                        if (rotatedImagePtr != NULL) {
+                            imgPtr = rotatedImagePtr;
+                            printf("  Rotated the image with an angle of %f degrees and background color %d.\n", angle, bgColor);
+                        } else {
+                            printf("  Failed to rotate the image with an angle of %f degrees and background color %d.\n", angle, bgColor);
+                        }
+                    }
+                } else {
+                    printf("  Transformation %s is not supported.\n", transformations[i]);
                 }
-            } else {
-                printf("  Transformation %s is not supported.\n", transformations[i]);
             }
         }
     }
